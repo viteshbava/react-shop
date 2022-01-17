dd400ea13661f1fd6e76c753d9c193eec80751e6
Vitesh Bava <vitesh.bava@gmail.com>
2022-01-18 12:51:02 +1300

    resonponsive navbar done for now

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,49 +2,82 @@ import React from "react";
 import { useState } from "react";
 import styles from "./Header.module.css";
 import Button, { BTN_TYPE } from "../UI/Button/Button";
+import Badge from "../UI/Badge/Badge";
 import Icon, { ICON_TYPE } from "../UI/Icon/Icon";
 
 const Header = () => {
-  const [showSideNav, setShowSideNav] = useState(false);
+  const [showMobileCollapseNav, setShowMobileCollapseNav] = useState(false);
 
   const hamburgerClickHandler = () => {
-    setShowSideNav((prev) => !prev);
+    setShowMobileCollapseNav((prev) => !prev);
   };
+
+  const navMobileCollapse = (
+    <nav className={styles["nav-mobile-collapse"]}>
+      <ul>
+        <li>
+          <button>
+            <Icon icon={ICON_TYPE.HEART_FULL} /> Wishlist (3)
+          </button>
+        </li>
+        <li>
+          <button>
+            <Icon icon={ICON_TYPE.SIGNOUT} /> Sign out
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
 
   return (
     <header>
       <div className={`container ${styles.wrapper}`}>
-        <button
-          className={styles["nav-mobile"]}
-          onClick={hamburgerClickHandler}
-        >
-          <Icon icon={showSideNav ? ICON_TYPE.TIMES : ICON_TYPE.HAMBURGER} />
-        </button>
+        <nav className={styles["nav-mobile-left"]}>
+          <button onClick={hamburgerClickHandler}>
+            <Icon
+              icon={
+                showMobileCollapseNav ? ICON_TYPE.TIMES : ICON_TYPE.HAMBURGER
+              }
+            />
+          </button>
+        </nav>
 
         <div className={styles.logo}>React Shop</div>
         <nav className={styles["nav-desktop"]}>
-          <Button style={BTN_TYPE.SECONDARY}>
-            <Icon icon={ICON_TYPE.SIGNOUT} />
-            Sign out
-          </Button>
-          <Button style={BTN_TYPE.SECONDARY}>
-            <Icon icon={ICON_TYPE.HEART_FULL} />
-            Wishlist (2)
-          </Button>
-          <Button style={BTN_TYPE.PRIMARY}>
-            <Icon icon={ICON_TYPE.CART} />
-            Cart (3)
-          </Button>
-          {/* <Button style={BTN_TYPE.PRIMARY}>
-            <Icon icon={ICON_TYPE.SIGNIN} />
-            Sign in
-          </Button> */}
+          <ul>
+            <li>
+              <Button style={BTN_TYPE.SECONDARY}>
+                <Icon icon={ICON_TYPE.SIGNOUT} />
+                Sign out
+              </Button>
+            </li>
+            <li>
+              <Button style={BTN_TYPE.SECONDARY}>
+                <Icon icon={ICON_TYPE.HEART_FULL} />
+                Wishlist (2)
+              </Button>
+            </li>
+            <li>
+              <Button style={BTN_TYPE.PRIMARY}>
+                <Icon icon={ICON_TYPE.CART} />
+                Cart (3)
+              </Button>
+            </li>
+            {/* <li>
+              <Button style={BTN_TYPE.PRIMARY}>
+                <Icon icon={ICON_TYPE.SIGNIN} />
+                Sign in
+              </Button>
+            </li> */}
+          </ul>
         </nav>
-        <nav className={styles["nav-mobile"]}>
-          <button>
+        <nav className={styles["nav-mobile-right"]}>
+          <button className={styles["nav-mobile-right__cart-icon"]}>
             <Icon icon={ICON_TYPE.CART} />
+            <Badge className={styles["nav-mobile-right__qty-badge"]}>99+</Badge>
           </button>
         </nav>
+        {showMobileCollapseNav ? navMobileCollapse : <></>}
       </div>
     </header>
   );

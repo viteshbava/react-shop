68491c30134167694fdb9fc3c15e74d5e3225801
Vitesh Bava <vitesh.bava@gmail.com>
2022-01-18 16:38:39 +1300

    product list responsive done for now

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -7,14 +7,14 @@ const ProductList = () => {
   return (
     <section>
       <SectionHeading>Products</SectionHeading>
-      <div className={styles["grid-wrapper"]}>
+      <ul className={styles["grid-wrapper"]}>
         <ProductListItem url="https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg" />
         <ProductListItem url="https://www.vishopper.com/images/products/maxxmax/PL/1090_cut-out-tall-and-thin-pine-tree.jpg" />
         <ProductListItem url="https://www.appears-itn.eu/wp-content/uploads/2018/07/long.jpg" />
         <ProductListItem url="https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg" />
         <ProductListItem url="https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg" />
         <ProductListItem url="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" />
-      </div>
+      </ul>
     </section>
   );
 };
--- src/components/ProductList/ProductListItem.js
+++ src/components/ProductList/ProductListItem.js
@@ -5,17 +5,19 @@ import Icon, { ICON_TYPE } from "../UI/Icon/Icon";
 
 const ProductListItem = ({ url }) => {
   return (
-    <Card className={styles.wrapper}>
-      <div className={styles["image-wrapper"]}>
-        <Icon
-          className={styles["wishlist-toggle"]}
-          icon={ICON_TYPE.HEART_EMPTY}
-        />
-        <img className={styles.image} src={url} alt="Product Image" />
-      </div>
-      <h2 className={styles.heading}>Compact Digital Camera</h2>
-      <h1 className={styles.price}>$99.90</h1>
-    </Card>
+    <li>
+      <Card className={styles.wrapper}>
+        <div className={styles["image-wrapper"]}>
+          <Icon
+            className={styles["wishlist-toggle"]}
+            icon={ICON_TYPE.HEART_EMPTY}
+          />
+          <img className={styles.image} src={url} alt="Product Image" />
+        </div>
+        <h2 className={styles.heading}>Compact Digital Camera</h2>
+        <h1 className={styles.price}>$99.90</h1>
+      </Card>
+    </li>
   );
 };
 

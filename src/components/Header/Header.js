a56a7147cc7e94dd9e11f20d1dc846ba75015870
Vitesh Bava <vitesh.bava@gmail.com>
2022-01-19 19:58:24 +1300

    sign in and sign out working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,51 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import styles from "./Header.module.css";
 import Button, { BTN_TYPE } from "../UI/Button/Button";
 import Badge from "../UI/Badge/Badge";
 import Icon, { ICON_TYPE } from "../UI/Icon/Icon";
+import AuthContext from "../../store/auth-context";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const [showMobileCollapseNav, setShowMobileCollapseNav] = useState(false);
+
+  const ctx = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const hamburgerClickHandler = () => {
     setShowMobileCollapseNav((prev) => !prev);
   };
 
+  const onSignOutHandler = () => {
+    ctx.onLogout();
+    setShowMobileCollapseNav(false);
+    navigate("/");
+  };
+
   const navMobileCollapse = (
     <nav className={styles["nav-mobile-collapse"]}>
       <ul>
-        <li>
-          <a href="/wishlist">
-            <Icon icon={ICON_TYPE.HEART_FULL} /> Wishlist (3)
-          </a>
-        </li>
-        <li>
-          <button>
-            <Icon icon={ICON_TYPE.SIGNOUT} /> Sign out
-          </button>
-        </li>
+        {ctx.isLoggedIn ? (
+          <>
+            <li>
+              <a href="/wishlist">
+                <Icon icon={ICON_TYPE.HEART_FULL} /> Wishlist (3)
+              </a>
+            </li>
+            <li>
+              <button onClick={onSignOutHandler}>
+                <Icon icon={ICON_TYPE.SIGNOUT} /> Sign out
+              </button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <a href="/signin">
+              <Icon icon={ICON_TYPE.SIGNIN} /> Sign in
+            </a>
+          </li>
+        )}
       </ul>
     </nav>
   );
@@ -47,30 +68,39 @@ const Header = () => {
 
         <nav className={styles["nav-desktop"]}>
           <ul>
-            {/* <li>
-              <Button style={BTN_TYPE.SECONDARY}>
-                <Icon icon={ICON_TYPE.SIGNOUT} />
-                Sign out
-              </Button>
-            </li>
-            <li>
-              <Button type="link" href="/wishlist" style={BTN_TYPE.SECONDARY}>
-                <Icon icon={ICON_TYPE.HEART_FULL} />
-                Wishlist (2)
-              </Button>
-            </li>
-            <li>
-              <Button type="link" href="/cart" style={BTN_TYPE.PRIMARY}>
-                <Icon icon={ICON_TYPE.CART} />
-                Cart (3)
-              </Button>
-            </li> */}
-            <li>
-              <Button type="link" href="/signin" style={BTN_TYPE.PRIMARY}>
-                <Icon icon={ICON_TYPE.SIGNIN} />
-                Sign in
-              </Button>
-            </li>
+            {ctx.isLoggedIn ? (
+              <>
+                <li>
+                  <Button style={BTN_TYPE.SECONDARY} onClick={onSignOutHandler}>
+                    <Icon icon={ICON_TYPE.SIGNOUT} />
+                    Sign out
+                  </Button>
+                </li>
+                <li>
+                  <Button
+                    type="link"
+                    href="/wishlist"
+                    style={BTN_TYPE.SECONDARY}
+                  >
+                    <Icon icon={ICON_TYPE.HEART_FULL} />
+                    Wishlist (2)
+                  </Button>
+                </li>
+                <li>
+                  <Button type="link" href="/cart" style={BTN_TYPE.PRIMARY}>
+                    <Icon icon={ICON_TYPE.CART} />
+                    Cart (3)
+                  </Button>
+                </li>
+              </>
+            ) : (
+              <li>
+                <Button type="link" href="/signin" style={BTN_TYPE.PRIMARY}>
+                  <Icon icon={ICON_TYPE.SIGNIN} />
+                  Sign in
+                </Button>
+              </li>
+            )}
           </ul>
         </nav>
         <nav className={styles["nav-mobile-right"]}>

2ceec68942b8b5d091a7a99116b850b7b63446c4
Vitesh Bava <vitesh.bava@gmail.com>
2022-02-23 10:45:24 +1300

    introduced lazy loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import "./App.css";
 
 import Layout from "./components/Layout/Layout";
+import PageLoader from "./components/Feedback/PageLoader/PageLoader";
 
 import { useDispatch } from "react-redux";
 import { fetchUserCart } from "./redux/actions/cart-actions";
@@ -12,18 +13,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import ScrollToTop from "./utilities/ScrollToTop";
 
-import SignIn from "./pages/Signin/SignIn";
-import Register from "./pages/Register";
 import ProductList from "./pages/ProductList/ProductList";
-import ProductSingle from "./pages/ProductSingle/ProductSingle";
-import Help from "./pages/Help/Help";
-import About from "./pages/About/About";
-import Wishlist from "./pages/Wishlist/Wishlist";
-import Cart from "./pages/Cart/Cart";
 import InfoError, { INFO_ERROR_TYPE } from "./pages/Error/InfoError";
-
-import AboutText_1 from "./pages/About/AboutText_1";
-import AboutText_2 from "./pages/About/AboutText_2";
+const Cart = React.lazy(() => import("./pages/Cart/Cart"));
+const SignIn = React.lazy(() => import("./pages/Signin/SignIn"));
+const Register = React.lazy(() => import("./pages/Register"));
+const ProductSingle = React.lazy(() =>
+  import("./pages/ProductSingle/ProductSingle")
+);
+const Help = React.lazy(() => import("./pages/Help/Help"));
+const About = React.lazy(() => import("./pages/About/About"));
+const Wishlist = React.lazy(() => import("./pages/Wishlist/Wishlist"));
+const AboutText_1 = React.lazy(() => import("./pages/About/AboutText_1"));
+const AboutText_2 = React.lazy(() => import("./pages/About/AboutText_2"));
 
 function App() {
   const dispatch = useDispatch();
@@ -40,32 +42,34 @@ function App() {
     <Router>
       <ScrollToTop />
       <Layout>
-        <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<ProductSingle />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/help/*" element={<Help />} />
-          <Route exact path="/about" element={<About />}>
-            <Route path="about1" element={<AboutText_1 />} />
-            <Route path="about2" element={<AboutText_2 />} />
-            <Route index element={<></>} />
-            <Route path="*" element={<div>About text not found!</div>} />
-          </Route>
-          <Route index element={<ProductList />} />
-          <Route
-            path="*"
-            element={
-              <InfoError
-                type={INFO_ERROR_TYPE.ERROR}
-                heading="Page Not Found!"
-                message="Please check the URL."
-              />
-            }
-          />
-        </Routes>
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/products/:id" element={<ProductSingle />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/help/*" element={<Help />} />
+            <Route exact path="/about" element={<About />}>
+              <Route path="about1" element={<AboutText_1 />} />
+              <Route path="about2" element={<AboutText_2 />} />
+              <Route index element={<></>} />
+              <Route path="*" element={<div>About text not found!</div>} />
+            </Route>
+            <Route index element={<ProductList />} />
+            <Route
+              path="*"
+              element={
+                <InfoError
+                  type={INFO_ERROR_TYPE.ERROR}
+                  heading="Page Not Found!"
+                  message="Please check the URL."
+                />
+              }
+            />
+          </Routes>
+        </Suspense>
       </Layout>
     </Router>
   );

b74c6eaae73244228db171a4149f04e5146ec9e8
Vitesh Bava <vitesh.bava@gmail.com>
2022-03-26 20:09:51 +1300

    fixed eslint warnigns in AuthRequired.js

--- src/components/AuthRequired.js
+++ src/components/AuthRequired.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { useLocation, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import PropTypes from 'prop-types';
+import { useLocation, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const AuthRequired = ({ children }) => {
   const location = useLocation();
   const isLoggedIn = useSelector((state) => state.auth.user);
 
-  return (
-    <>
-      {isLoggedIn ? (
-        children
-      ) : (
-        <Navigate to="/signin" state={{ from: location }} replace />
-      )}
-    </>
+  return isLoggedIn ? (
+    children
+  ) : (
+    <Navigate to="/signin" state={{ from: location }} replace />
   );
+};
+
+AuthRequired.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthRequired;

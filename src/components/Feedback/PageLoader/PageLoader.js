57baba2d882cf7a7e69cae4b29461ab97ca4f675
Vitesh Bava <vitesh.bava@gmail.com>
2022-03-26 17:44:48 +1300

    fixed eslint warnings in PageLoader.js

--- src/components/Feedback/PageLoader/PageLoader.js
+++ src/components/Feedback/PageLoader/PageLoader.js
@@ -1,8 +1,8 @@
-import styles from "./PageLoader.module.css";
-import Spinner from "../../UI/Spinner/Spinner";
+import styles from './PageLoader.module.css';
+import Spinner from '../../UI/Spinner/Spinner';
 
-const PageLoader = () => {
-  return <Spinner className={styles.main} width={"7rem"} thinkness={"1rem"} />;
-};
+const PageLoader = () => (
+  <Spinner className={styles.main} width="7rem" thinkness="1rem" />
+);
 
 export default PageLoader;

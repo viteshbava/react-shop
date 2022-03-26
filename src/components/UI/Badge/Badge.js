b76b77b61aec48717d911bb0759c3d09c8334ce1
Vitesh Bava <vitesh.bava@gmail.com>
2022-03-26 20:12:51 +1300

    fixed esline warnings in badge componenet

--- src/components/UI/Badge/Badge.js
+++ src/components/UI/Badge/Badge.js
@@ -1,10 +1,18 @@
-import React from "react";
-import styles from "./Badge.module.css";
+import PropTypes from 'prop-types';
+import styles from './Badge.module.css';
 
 const Badge = ({ className, children }) => {
   let badgeStyles = styles.wrapper;
   if (className) badgeStyles = `${badgeStyles} ${className}`;
   return <div className={badgeStyles}>{children}</div>;
+};
+
+Badge.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+Badge.defaultProps = {
+  className: null,
 };
 
 export default Badge;

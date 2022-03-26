3a84af7b13b7c24919b57bba4b56a1a99ee816da
Vitesh Bava <vitesh.bava@gmail.com>
2022-03-27 12:53:12 +1300

    fixed eslint warnings in hamburger.js

--- src/components/UI/Hamburger/Hamburger.js
+++ src/components/UI/Hamburger/Hamburger.js
@@ -1,15 +1,27 @@
-import styles from "./Hamburger.module.css";
+import PropTypes from 'prop-types';
+import styles from './Hamburger.module.css';
 
-const Hamburger = ({ onClick, className, rowHeight = 2 }) => {
-  const buttonClasses = styles.wrapper + (className ? ` ${className}` : "");
+const Hamburger = ({ onClick, className, rowHeight }) => {
+  const buttonClasses = styles.wrapper + (className ? ` ${className}` : '');
   const height = { height: `${rowHeight}px` };
   return (
-    <button onClick={onClick} className={buttonClasses}>
-      <span style={height} className={styles.row}></span>
-      <span style={height} className={styles.row}></span>
-      <span style={height} className={styles.row}></span>
+    <button type="button" onClick={onClick} className={buttonClasses}>
+      <span style={height} className={styles.row} />
+      <span style={height} className={styles.row} />
+      <span style={height} className={styles.row} />
     </button>
   );
+};
+
+Hamburger.propTypes = {
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  rowHeight: PropTypes.number,
+};
+Hamburger.defaultProps = {
+  onClick: () => {},
+  className: null,
+  rowHeight: 2,
 };
 
 export default Hamburger;

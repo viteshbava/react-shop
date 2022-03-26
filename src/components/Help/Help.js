dadb6ed98c74471adbb9628f5100d4adf59929a6
Vitesh Bava <vitesh.bava@gmail.com>
2022-03-26 17:46:45 +1300

    fixed eslint warnings in Help componenet

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,13 +1,13 @@
-import styles from "./Help.module.css";
-import HelpTextOne from "./HelpTextOne";
-import HelpTexTwo from "./HelpTextTwo";
-import { Routes, Route } from "react-router-dom";
-import SectionHeading from "../../components/UI/SectionHeading/SectionHeading";
-import Button from "../../components/UI/Button/Button";
-import useSetDocumentTitle from "../../hooks/use-setDocumentTitle";
+import { Routes, Route } from 'react-router-dom';
+import styles from './Help.module.css';
+import HelpTextOne from './HelpTextOne';
+import HelpTexTwo from './HelpTextTwo';
+import SectionHeading from '../UI/SectionHeading/SectionHeading';
+import Button from '../UI/Button/Button';
+import useSetDocumentTitle from '../../hooks/use-setDocumentTitle';
 
 const Help = () => {
-  useSetDocumentTitle("Help");
+  useSetDocumentTitle('Help');
 
   return (
     <section className={styles.wrapper}>
@@ -16,14 +16,14 @@ const Help = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </div>
-      <nav className={styles["help-nav"]}>
-        <Button link={"help1"}>Help Text 1</Button>
-        <Button link={"help2"}>Help Text 2</Button>
+      <nav className={styles['help-nav']}>
+        <Button link="help1">Help Text 1</Button>
+        <Button link="help2">Help Text 2</Button>
       </nav>
       <Routes>
         <Route path="help1" element={<HelpTextOne />} />
         <Route path="help2" element={<HelpTexTwo />} />
-        <Route index element={<></>} />
+        <Route index element={null} />
         <Route path="*" element={<div>Help text not found!</div>} />
       </Routes>
     </section>

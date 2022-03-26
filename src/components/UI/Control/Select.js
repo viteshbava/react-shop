7fc3d8f3d4be1b3d533d89871e8ff15710eb9ab8
Vitesh Bava <vitesh.bava@gmail.com>
2022-03-27 12:03:30 +1300

    fixed eslint warnings in Select.js

--- src/components/UI/Control/Select.js
+++ src/components/UI/Control/Select.js
@@ -1,31 +1,31 @@
-import Chevron from "./ControlComponents/Chevron.svg";
-import ControlWrapper from "./ControlComponents/ControlWrapper";
-import styles from "./Select.module.css";
+import PropTypes from 'prop-types';
+import Chevron from './ControlComponents/Chevron.svg';
+import ControlWrapper from './ControlComponents/ControlWrapper';
+import styles from './Select.module.css';
 
 const Select = ({
+  id,
   invalid,
   label,
   feedback,
   className,
   options,
   focusRef,
-  ...props
 }) => {
-  props.ref = focusRef;
   const selectStyle = { backgroundImage: `url(${Chevron})` };
 
   if (!options || options.length === 0)
-    console.error("No options supplied to Select.js UI component!");
+    console.error('No options supplied to Select.js UI component!');
 
   return (
     <ControlWrapper
-      id={props.id}
+      id={id}
       invalid={invalid}
       label={label}
       feedback={feedback}
       className={className}
     >
-      <select className={styles.select} style={selectStyle} {...props}>
+      <select className={styles.select} style={selectStyle} ref={focusRef}>
         {options.map((opt) => (
           <option key={opt} value={opt}>
             {opt}
@@ -34,6 +34,26 @@ const Select = ({
       </select>
     </ControlWrapper>
   );
+};
+
+Select.propTypes = {
+  id: PropTypes.string.isRequired,
+  invalid: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  feedback: PropTypes.string,
+  className: PropTypes.string,
+  focusRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(HTMLInputElement) }),
+  ]),
+  options: PropTypes.arrayOf(PropTypes.string),
+};
+Select.defaultProps = {
+  invalid: false,
+  feedback: null,
+  className: null,
+  focusRef: null,
+  options: [],
 };
 
 export default Select;

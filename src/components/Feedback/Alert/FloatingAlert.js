026a96fc4ad3c28c54c3be6ebd0638efe7403d27
Vitesh Bava <vitesh.bava@gmail.com>
2022-04-03 20:01:18 +1200

    abstracted some more into custom hook

--- src/components/Feedback/Alert/FloatingAlert.js
+++ src/components/Feedback/Alert/FloatingAlert.js
@@ -9,26 +9,25 @@ import styles from './FloatingAlert.module.css';
 import useAnimateEnter from '../../../hooks/use-animateEnter';
 import useAnimateExit from '../../../hooks/use-animateExit';
 
-const useDelayExit = ({ onExit }) => {
+const useDelayExit = () => {
   const [isExiting, setIsExiting] = useState(false);
+  const [exitingDone, setExitingDone] = useState(false);
 
   useEffect(() => {
     let timeoutId;
     if (isExiting) {
-      timeoutId = setTimeout(onExit, 500);
+      timeoutId = setTimeout(() => setExitingDone(true), 500);
     }
     return () => clearTimeout(timeoutId);
-  }, [isExiting, onExit]);
+  }, [isExiting]);
 
-  return { isExiting, setIsExiting: () => setIsExiting(true) };
+  return { setIsExiting: () => setIsExiting(true), isExiting, exitingDone };
 };
 
 const FloatingAlert = ({ alert }) => {
   const dispatch = useDispatch();
   const [isEntering, setIsEntering] = useState(true);
-  const { isExiting, setIsExiting } = useDelayExit({
-    onExit: () => dispatch(uiActions.removeAlert(alert.id)),
-  });
+  const { setIsExiting, isExiting, exitingDone } = useDelayExit();
 
   useEffect(() => {
     let timeoutId;
@@ -41,6 +40,10 @@ const FloatingAlert = ({ alert }) => {
   let animateStyle = '';
   if (isEntering) animateStyle = styles['enter-animate'];
   if (isExiting) animateStyle = styles['exit-animate'];
+
+  useEffect(() => {
+    if (exitingDone) dispatch(uiActions.removeAlert(alert.id));
+  }, [exitingDone, alert.id, dispatch]);
 
   const onClickHandler = () => {
     setIsExiting();

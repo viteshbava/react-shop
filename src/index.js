9cd85df3cf5362bdc34bdee0e09cc5f9648c1562
Vitesh Bava <vitesh.bava@gmail.com>
2022-04-15 19:01:36 +1200

    test revert

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
 import { ModalContextProvider } from './context/modal-context';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ModalContextProvider>
         <App />
       </ModalContextProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
